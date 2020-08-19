80b07488bad54e660560fe42b40a60c1ad0d100c
unknown <rennerpa93@gmail.com>
2020-08-19 19:04:58 -0400

    Project Structure Commit

---
+++ RevFlix.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RevFlix.Client.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
