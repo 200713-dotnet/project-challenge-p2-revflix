06e1d3c31a02c2cd0f65c5d7e5a6e409812ce3df
lyndon santos <santos.lyndon@gmail.com>
2020-08-19 21:45:25 -0700

    search by title implemented

---
+++ RevFlix.Service/Controllers/Models/MovieImdbModelBase.cs
@@ -0,0 +1,14 @@
+namespace RevFlix.Service.Controllers.Models
+{
+  public class MovieImdbModelBase
+  {
+    public string Title { get; set; }
+    // public int Year { get; set; }
+    public string Imdb_id { get; set; }
+
+    public MovieImdbModelBase()
+    {
+
+    }
+  }
+}
